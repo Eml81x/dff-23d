1eaf03273cf33d3a8f32f69632f196fa1d8352e2
Eml81x <152807573+Eml81x@users.noreply.github.com>
2023-12-24 00:27:51 +0200

    Update load.ps1

--- load.ps1
+++ load.ps1
@@ -1,10 +1,10 @@
-Invoke-WebRequest -Uri https://nightly.link/UltimMC/Launcher/workflows/main/develop/mmc-cracked-win32.zip -OutFile test.zip %userprofile%/Downloads
+Invoke-WebRequest -Uri https://nightly.link/UltimMC/Launcher/workflows/main/develop/mmc-cracked-win32.zip -OutFile test.zip $env:userprofile/Downloads
 Invoke-WebRequest -Uri https://github.com/pal1000/mesa-dist-win/releases/download/23.3.1/mesa3d-23.3.1-release-msvc.7z -OutFile %userprofile%/Downloads
 choco install corretto17jdk -y
 
 mkdir %userprofile%/AppData/Roaming/.minecraft/mods
-Invoke-WebRequest -Uri https://cdn.modrinth.com/data/P7dR8mSH/versions/fdestf2d/fabric-api-0.91.2%2B1.20.2.jar -OutFile api.jar -OutFile %userprofile%/AppData/Roaming/.minecraft/mods/api.jar
-Invoke-WebRequest -Uri https://cdn.modrinth.com/data/AANobbMI/versions/pmgeU5yX/sodium-fabric-mc1.20.2-0.5.5.jar -OutFile sodium.jar -OutFile %userprofile%/AppData/Roaming/.minecraft/mods/sodium.jar
-Invoke-WebRequest -Uri https://www.curseforge.com/api/v1/mods/308892/files/4789765/download -OutFile litematica.jar -OutFile %userprofile%/AppData/Roaming/.minecraft/mods/litematica.jar
-Invoke-WebRequest -Uri https://www.curseforge.com/api/v1/mods/303119/files/4788432/download -OutFile malilib.jar -OutFile %userprofile%/AppData/Roaming/.minecraft/mods/malilib.jar
-Invoke-WebRequest -Uri https://cdn.modrinth.com/data/ITCqumHN/versions/cxKkZJ6h/litematica-printer-mc1.20.2-7.0.9.jar -OutFile %userprofile%/AppData/Roaming/.minecraft/mods/printer.jar
+Invoke-WebRequest -Uri https://cdn.modrinth.com/data/P7dR8mSH/versions/fdestf2d/fabric-api-0.91.2%2B1.20.2.jar -OutFile api.jar -OutFile $env:appdata\.minecraft\mods\api.jar
+Invoke-WebRequest -Uri https://cdn.modrinth.com/data/AANobbMI/versions/pmgeU5yX/sodium-fabric-mc1.20.2-0.5.5.jar -OutFile sodium.jar -OutFile $env:appdata\.minecraft\mods\sodium.jar
+Invoke-WebRequest -Uri https://www.curseforge.com/api/v1/mods/308892/files/4789765/download -OutFile litematica.jar -OutFile $env:appdata\.minecraft\mods\litematica.jar
+Invoke-WebRequest -Uri https://www.curseforge.com/api/v1/mods/303119/files/4788432/download -OutFile malilib.jar -OutFile $env:appdata\.minecraft\mods\malilib.jar
+Invoke-WebRequest -Uri https://cdn.modrinth.com/data/ITCqumHN/versions/cxKkZJ6h/litematica-printer-mc1.20.2-7.0.9.jar -OutFile $env:appdata\.minecraft\mods\printer.jar
