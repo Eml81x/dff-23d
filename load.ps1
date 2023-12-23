47900b045fce9043a847d0387351cea09b0f4143
Eml81x <152807573+Eml81x@users.noreply.github.com>
2023-12-24 00:44:04 +0200

    Update load.ps1

--- load.ps1
+++ load.ps1
@@ -2,7 +2,7 @@ Invoke-WebRequest -Uri https://nightly.link/UltimMC/Launcher/workflows/main/deve
 Invoke-WebRequest -Uri https://github.com/pal1000/mesa-dist-win/releases/download/23.3.1/mesa3d-23.3.1-release-msvc.7z -OutFile $env:userprofile\Downloads\drivers.zip
 choco install corretto17jdk -y
 
-mkdir %userprofile%/AppData/Roaming/.minecraft/mods
+mkdir $env:appdata\.minecraft\mods\
 Invoke-WebRequest -Uri https://cdn.modrinth.com/data/P7dR8mSH/versions/fdestf2d/fabric-api-0.91.2%2B1.20.2.jar -OutFile $env:appdata\.minecraft\mods\api.jar
 Invoke-WebRequest -Uri https://cdn.modrinth.com/data/AANobbMI/versions/pmgeU5yX/sodium-fabric-mc1.20.2-0.5.5.jar -OutFile $env:appdata\.minecraft\mods\sodium.jar
 Invoke-WebRequest -Uri https://www.curseforge.com/api/v1/mods/308892/files/4789765/download -OutFile $env:appdata\.minecraft\mods\litematica.jar
